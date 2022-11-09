e8f34fd7f9c52adb15f255fb43243419d9673129
dbrandone <115263503+dbrandone@users.noreply.github.com>
2022-11-09 09:09:48 -0600

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -4,11 +4,11 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 // modify these settings according to the account on your database server.
-$host = "localhost";
+$host = "127.0.0.1:54138";
 $port = "3306";
-$username = "root";
-$user_pass = "root";
-$database_in_use = "lunaristechadmin";
+$username = "azure";
+$user_pass = "6#vWHD_$";
+$database_in_use = "employee_application";
 
 #$mysqli = mysqli_connect($host, $username, $user_pass, $database_in_use);
 $mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
